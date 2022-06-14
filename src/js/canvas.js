c5e147eb1219b72f1c6f579b8d888be9ead3840d
IbsanjU <bharath2668@gmail.com>
2022-06-14 09:48:39 +0530

    add more colors

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -11,7 +11,17 @@ const mouse = {
 	y: innerHeight / 2
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66',
+				'#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+				'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+				'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+				'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+				'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+				'#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+				'#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+				'#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+				'#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+				'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 const gravity = 1
 const friction = 0.99
 
@@ -81,7 +91,7 @@ class Ball {
 let ballArray = []
 function init() {
 	ballArray = []
-	for (let i = 0; i < 50; i++) {
+	for (let i = 0; i < 100; i++) {
 		const radius = randomIntFromRange(5, 30)
 		const x = randomIntFromRange(radius, canvas.width - radius)
 		const y = randomIntFromRange(0, canvas.height - radius)
@@ -98,7 +108,7 @@ function animate() {
 	requestAnimationFrame(animate)
 	c.clearRect(0, 0, canvas.width, canvas.height)
 
-	c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
+	c.fillText('IbsanjU - CLICK TO RESET', mouse.x, mouse.y)
 	ballArray.forEach((object) => {
 		object.update()
 	})
